7067b12831391e872ec8c2c7a7217bec6c2e7cda
GLOBAL\ethan.wicks <ethanowicks9@gmail.com>
2026-01-12 14:03:50 +0000

    set up neon db and replace it with local db

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'release_management_system',
-    user: 'postgres',
-    password: 'postgres'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
 });
 
 module.exports = pool;
