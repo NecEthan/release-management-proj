1a7fbcd230ea2671dd4d20a22e3a25785819e482
GLOBAL\ethan.wicks <ethanowicks9@gmail.com>
2026-01-23 14:20:54 +0000

    configure cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ require('./cron-jobs/daily-job');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get('/health', (req, res) => {
